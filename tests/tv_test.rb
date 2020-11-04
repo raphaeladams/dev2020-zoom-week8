25b9105ec6dbc11b821588dca007d5bda9b657ee
Raphael Adams <raphael.adams@shopify.com>
2020-11-04 16:42:01 -0500

    Added tests for TV class, very short!

---
+++ tests/tv_test.rb
@@ -0,0 +1,17 @@
+# Accessorize
+# Raphael Adams
+
+require 'minitest/autorun'
+require '../tv'
+
+class TVTest < Minitest::Test
+
+  def setup
+    @tv = TV.new("QLED Smart", "Samsung", "50")
+  end
+
+  def test_to_s
+    assert_output("A 50-in QLED Smart TV made by Samsung\n") { @tv.to_s }
+  end
+
+end
